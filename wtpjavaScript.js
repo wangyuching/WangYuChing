5571ce71fc5fda25057c5ed2b302674cb6ff1700
昱晴 王 <5b1g0023@stust.edu.tw>
2025-04-05 22:09:02 +0800

    combi2Hw

---
+++ wtpjavaScript.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const levelSelect = document.getElementById('level-select')
+    const playButton = document.getElementById('play-button');
+    const gameContainer = document.getElementById('game-container');
+
+    const tubes = [];
+    let levelCount = 1;
+
+    levelSelect.addEventListener('change', (event)=>{ /*evevt 觸發事件的相關參數。可自行取名*/
+        const selectedLevel = parseInt(event.target.value, 10); // 將選中的值（字串）轉換為整數(是下拉選單中選中的值, 十進位制)
+        chooseLevel(selectedLevel);
+    })
+
+    function chooseLevel(Level){
+        levelCount = Level;
+        document.getElementById('level-count').textContent = levelCount;
+    }
+    
+    playButton.addEventListener('click', ()=>{
+        alert('Start Game')
+    });
+});
