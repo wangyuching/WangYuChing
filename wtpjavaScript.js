fc2966d6f7f4075fd3ed396aff5edfac99678d98
wangyuching <5b1g0023@stust.edu.tw>
2025-04-09 11:44:11 +0800

    0

--- wtpjavaScript.js
+++ wtpjavaScript.js
@@ -6,16 +6,16 @@ document.addEventListener('DOMContentLoaded',()=>{
     const tubes = [];
     let levelCount = 1;
 
+    function chooseLevel(Level){
+        levelCount = Level;
+        document.getElementById('level-count').textContent = levelCount;
+    }
+
     levelSelect.addEventListener('change', (event)=>{ /*evevt 觸發事件的相關參數。可自行取名*/
         const selectedLevel = parseInt(event.target.value, 10); // 將選中的值（字串）轉換為整數(是下拉選單中選中的值, 十進位制)
         chooseLevel(selectedLevel);
     })
 
-    function chooseLevel(Level){
-        levelCount = Level;
-        document.getElementById('level-count').textContent = levelCount;
-    }
-    
     playButton.addEventListener('click', ()=>{
         alert('Start Game')
     });
